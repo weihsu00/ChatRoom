d8bd935e3a6a0f5b3fa4dde1b5ff938b43b45514
Shan-Wei Hsu <ericshe21110@gmail.com>
2018-01-25 15:05:33 +0800

    chat room prototype

---
+++ app/src/main/java/com/example/wei/winterapp/ChatMessage.java
@@ -0,0 +1,49 @@
+package com.example.wei.winterapp;
+
+import java.util.Date;
+
+/**
+ * Created by wei on 2018/1/25.
+ */
+
+public class ChatMessage {
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        // Initialize to current time
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage(){
+
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
